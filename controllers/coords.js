687bd55b4087e900a139b78e7974d816f61a7e5c
Sulaiman Barki <99899527+sulaimanbarki@users.noreply.github.com>
2023-06-03 01:06:15 +0500

    image

--- controllers/coords.js
+++ controllers/coords.js
@@ -1,21 +1,46 @@
 const polutionModel = require("../models/polution");
 const dummydata = require("../dumyData");
+const cloudinary = require("cloudinary").v2;
+const fs = require('fs');
+
+cloudinary.config({
+  cloud_name: "dfjfmkeha",
+  api_key: "889875147537854",
+  api_secret: "O2NOsf5pgcjimwkbjxeh0-hfCI0",
+});
+
 
 const readCoordsFile = async (req, res) => {
   const image_url = req.file.path;
+  const cloudinaryRes = cloudinary.uploader.upload(image_url, {
+    public_id: "funadiq",
+  });
+
   const { latitude, longitude, address } = req.body;
   try {
-    const result = await polutionModel.collection.insertOne({
-      image_url: image_url,
-      latitude: latitude,
-      longitude: longitude,
-      address: address,
-    });
-    console.log(result);
-    res.status(200).json({
-      msg: "Data uploaded succesfully",
-    });
-  } catch (error) {}
+
+    cloudinaryRes
+      .then(async (data) => {
+
+        const result = await polutionModel.collection.insertOne({
+          image_url: data.secure_url,
+          latitude: latitude,
+          longitude: longitude,
+          address: address,
+        });
+        console.log(result);
+        res.status(200).json({
+          msg: "Data uploaded succesfully",
+        });
+
+
+      }).catch((err) => {
+        res.send({ msgErr: "An error has ocurred." })
+      });
+
+
+
+  } catch (error) { }
 };
 
 const getData = async (req, res) => {
